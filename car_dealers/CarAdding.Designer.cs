ab599ee4e2feb7ffc638df3b18694f844b49e7e6
Jan <fiszer@student.agh.edu.pl>
2022-04-05 21:26:40 +0200

    image visualisation improved

--- car_dealers/CarAdding.Designer.cs
+++ car_dealers/CarAdding.Designer.cs
@@ -166,7 +166,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(283, 24);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(364, 174);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
